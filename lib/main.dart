cdc77bce85a4830252864ea7102ba99ea86edcaa
Sergey Pashnin <sspinit88@gmail.com>
2021-07-14 09:22:09 +0300

    i am rich

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[900],
+        appBar: AppBar(
+          title: Text('I am rich!!!'),
+          backgroundColor: Colors.blueGrey[800],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
